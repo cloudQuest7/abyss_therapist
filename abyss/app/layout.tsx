32533534763177a21e3d814d86836565f9fd5789
cloudQuest7 <anjalijayakumar79@gmail.com>
2025-12-28 01:32:55 +0530

    analytics setup

--- abyss/app/layout.tsx
+++ abyss/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from '@/components/AuthContext'
 import "./globals.css";
 import localFont from 'next/font/local';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="antialiased">
        <AuthProvider>
           {children}
+           <Analytics />
         </AuthProvider>
       </body>
     </html>
